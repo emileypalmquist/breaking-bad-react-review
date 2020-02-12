1fe16aefee67d8337b51b79f4462319a8054bae1
emileypalmquist <emiley.palmquist@icloud.com>
2020-02-12 14:20:57 -0800

    initial commit

---
+++ src/components/CharacterInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CharacterInfo = props => {
+  return (
+    <div >
+      Character Info
+    </div>
+  );
+};
+
+export default CharacterInfo;
---
+++ src/containers/CharactersPage.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+
+
+// https://www.breakingbadapi.com/api/characters
+
+
+class CharactersPage extends Component {
+
+    render() {
+        return (
+            <div>
+                Characters Page
+            </div>
+        )
+    }
+}
+
+export default CharactersPage
